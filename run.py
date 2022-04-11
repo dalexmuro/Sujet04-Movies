34f299c50a9c90ea04d8257f637cf7f8eb36aeb5
wrzdylan <wrzdylan@gmail.com>
2022-04-11 23:02:01 +0200

    films and genres pages

--- run.py
+++ run.py
@@ -3,6 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from ast import literal_eval
 import random
+import squarify
+import calendar
 
 df_movies = pd.DataFrame(pd.read_csv("data/highest_hollywood_grossing_movies.csv")).iloc[:, 1:]
 
@@ -104,18 +106,121 @@ def dist():
         }
     })
 
+
+def films_quantity_by_year():
+    fig, ax = plt.subplots( nrows=1, ncols=1, figsize=(20,10) )
+
+    groupedByDate = df_movies.groupby(df_movies["Release Date"].dt.year).size()
+
+    ax = plt.plot(groupedByDate.index, groupedByDate.values);
+
+    fig.savefig('./static/images/filmsQuantityByYear.png', bbox_inches = 'tight')
+
+    plt.close(fig)
+    
+    legend = "Quantité de film par année"
+    
+    # Embed the result in the html output.
+    return {
+        "img": "static/images/filmsQuantityByYear.png",
+        "legend": legend 
+    }
+    
+
+def films_quantity_by_month():
+    fig, ax = plt.subplots( nrows=1, ncols=1, figsize=(20,10) )
+
+    plt.axis('off')
+
+    groupedByMonth = df_movies.groupby(df_movies["Release Date"].dt.month).size()
+
+    labels = [f"{calendar.month_name[i+1]} : {v}" for i, v in enumerate(groupedByMonth)]
+
+    squarify.plot(sizes=groupedByMonth.values, label=labels, alpha=.8, text_kwargs={'fontsize':18}, pad=True);
+
+    fig.savefig('./static/images/filmsQuantityByMonth.png', bbox_inches = 'tight')
+
+    plt.close(fig)
+
+    legend = "Quantité de films sortis par mois"
+
+    return {
+        "img": "static/images/filmsQuantityByMonth.png",
+        "legend": legend 
+    }
+
+
 @app.route('/films')
 def films():
+    pathFilmsQuantityByYear = films_quantity_by_year()
+    pathFilmsQuantityByMonth = films_quantity_by_month()
     return render_template(
         'films.html',
-        data={'msg': "Films !"}
+        data={
+                'filmsQuantityByYear': {
+                    'title': 'Quantité de film par année',
+                    'path': pathFilmsQuantityByYear['img'],
+                    'legend': pathFilmsQuantityByYear['legend'],
+                    'link': 'filmsQuantityByYear'
+                },
+                'pathFilmsQuantityByMonth': {
+                    'title': 'Quantité de films sortis par mois',
+                    'path': pathFilmsQuantityByMonth['img'],
+                    'legend': pathFilmsQuantityByMonth['legend'],
+                    'link': 'pathFilmsQuantityByMonth'
+                }
+            }
     )
+
+
+def films_quantity_by_genres():
+    fig, ax = plt.subplots( nrows=1, ncols=1, figsize=(20,10) )
+
+    genres = pd.DataFrame(df_movies["Genre"].explode().value_counts()).rename(columns={'Genre': 'Quantity'})
+
+    circle = plt.Circle((0,0), 0.7, color='white')
+
+    explode = [0.1 if v == "Adventure" else 0 for v in genres.index]
+
+    ax.pie(genres.Quantity, labels=genres.index, explode=explode, autopct='%1.1f%%', wedgeprops = { 'linewidth' : 2, 'edgecolor' : 'white' });
+
+    p = plt.gcf()
+
+    p.gca().add_artist(circle);
+
+    fig.savefig('./static/images/filmsQuantityByGenres.png', bbox_inches = 'tight')
+
+    plt.close(fig)
+
+    legend = "Quantité de films par genres"
+
+    return {
+        "img": "static/images/filmsQuantityByGenres.png",
+        "legend": legend 
+    }
+
+
+# def sales_by_genres():
+#     genres['Domestic Sales'] = [df_movies[df_movies["Genre"].apply(lambda x : genre in x)]['Domestic Sales (in $)'].sum() for genre in genres.index]
+#     genres['International Sales'] = [df_movies[df_movies["Genre"].apply(lambda x : genre in x)]['International Sales (in $)'].sum() for genre in genres.index]
+#     genres['World Sales'] = [df_movies[df_movies["Genre"].apply(lambda x : genre in x)]['World Sales (in $)'].sum() for genre in genres.index]
+        
+
+
 
 @app.route('/genres')
 def genres():
+    pathFilmsQuantityByGenres = films_quantity_by_genres()
     return render_template(
         'genres.html',
-        data={'msg': "Genres !"}
+        data={
+            'filmsQuantityByGenres': {
+                'title': 'Quantité de films par genres',
+                'path': pathFilmsQuantityByGenres['img'],
+                'legend': pathFilmsQuantityByGenres['legend'],
+                'link': 'filmsQuantityByGenres'
+            }
+        }
     )
 
 @app.cli.command()
